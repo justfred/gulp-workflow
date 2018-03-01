d834f7dffb1a635e92f41c6ce351944c4efa2de8
Frederick Heald <fheald@bandwidthx.com>
2018-02-28 22:15:30 -0800

    Begin working on gulpfile; browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,34 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+const SOURCEPATHS = {
+  sassSource: 'src/scss/*.scss'
+}
+
+const APPPATH = {
+  root: 'app/',
+  css: 'app/css',
+  js: 'app/js'
+}
 
 gulp.task('sass', function() {
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', ['sass'], function() {
+    browserSync.init([
+      APPPATH.css + '/*.css',
+      APPPATH.root + '/*.html',
+      APPPATH.js + '/*.js',
+    ], {
+      server: {
+        baseDir: APPPATH.root
+      }
+    });
+});
+
+gulp.task('default', ['serve']);
