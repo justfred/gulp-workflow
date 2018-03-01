2596ab73f357d84a2fc5ccc3b2ec347b3eba0e7a
Frederick Heald <fheald@bandwidthx.com>
2018-03-01 08:57:36 -0800

    copy tast in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
+const autoprefixer = require('gulp-autoprefixer');
 
 const SOURCEPATHS = {
-  sassSource: 'src/scss/*.scss'
+  sassSource: 'src/scss/*.scss',
+  htmlSource: 'src/*.html'
 }
 
 const APPPATH = {
@@ -13,12 +15,25 @@ const APPPATH = {
   js: 'app/js'
 }
 
+//process sass scss into css
 gulp.task('sass', function() {
   return gulp.src(SOURCEPATHS.sassSource)
+  .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    //compact/expanded/compressed/nested/expanded
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest(APPPATH.css));
 });
 
+//copy html files
+gulp.task('copy', function() {
+    gulp.src(SOURCEPATHS.htmlSource)
+      .pipe(gulp.dest(APPPATH.root))
+});
+
+//init browserSync
 gulp.task('serve', ['sass'], function() {
     browserSync.init([
       APPPATH.css + '/*.css',
@@ -32,8 +47,9 @@ gulp.task('serve', ['sass'], function() {
 });
 
 //watch these directories and run task when anything changes
-gulp.task('watch', ['serve', 'sass'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 });
 
 gulp.task('default', ['watch']);
