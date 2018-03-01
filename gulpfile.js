d52868d8117514e2bfeb0eb2f8b027f32135c2ba
Frederick Heald <fheald@bandwidthx.com>
2018-02-28 21:59:05 -0800

    gulp-sass and gulp added

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('src/scss/app.scss')
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('default', ['sass']);
